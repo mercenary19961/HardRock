78a5e34802b4dc5c530ff3631f896591bb169097
Zaid Sabbagh <sabbaghzaid88@gmail.com>
2025-12-18 16:15:54 +0300

    feat: adding google tag manager to head and body

--- resources/js/pages/Landing.tsx
+++ resources/js/pages/Landing.tsx
@@ -11,10 +11,27 @@ export default function Landing() {
     return (
         <>
             <Head title="Home">
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-TJTKSH9J');`
+                    }}
+                />
             </Head>
             
             <SmoothScroll>
                 <div className="min-h-screen bg-background text-foreground font-sans antialiased selection:bg-primary/20 selection:text-primary">
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-TJTKSH9J"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
                     <Navbar />
                     
                     <main>
