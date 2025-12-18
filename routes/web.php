6406a44a2eaa86ed458c1c3bd53b2e52cb939a86
Zaid Sabbagh <sabbaghzaid88@gmail.com>
2025-12-18 14:20:06 +0300

    fix: remove temporary admin creation route

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,6 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-
-// TEMPORARY: One-time admin creation route - REMOVE AFTER USE!
-Route::get('/create-admin-once', function () {
-    $user = User::where('email', 'admin@hardrock.com')->first();
-
-    if ($user) {
-        $user->password = Hash::make('password');
-        $user->is_admin = true;
-        $user->save();
-        return 'Admin user updated! Email: admin@hardrock.com - DELETE THIS ROUTE NOW!';
-    }
-
-    User::create([
-        'name' => 'Admin',
-        'email' => 'admin@hardrock.com',
-        'password' => Hash::make('password'),
-        'is_admin' => true,
-    ]);
-
-    return 'Admin user created! Email: admin@hardrock.com - DELETE THIS ROUTE NOW!';
-});
 
 // Landing Page (Public)
 Route::get('/', function () {
