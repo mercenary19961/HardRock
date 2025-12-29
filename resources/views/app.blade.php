63fd77c0bbe99b2aaa3a50e153f58d6db62dd703
Zaid Sabbagh <sabbaghzaid88@gmail.com>
2025-12-29 09:48:45 +0300

    feat: added linkedin partner API

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -86,6 +86,31 @@
         </noscript>
         @endif
         <!-- End Meta Pixel Code -->
+
+        <!-- LinkedIn Insight Tag -->
+        @if(config('services.linkedin.partner_id'))
+        <script type="text/javascript">
+            _linkedin_partner_id = "{{ config('services.linkedin.partner_id') }}";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        </script>
+        <script type="text/javascript">
+            (function(l) {
+                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+        </script>
+        <noscript>
+            <img height="1" width="1" style="display:none;" alt=""
+                 src="https://px.ads.linkedin.com/collect/?pid={{ config('services.linkedin.partner_id') }}&fmt=gif" />
+        </noscript>
+        @endif
+        <!-- End LinkedIn Insight Tag -->
     </head>
     <body class="font-sans antialiased">
         @inertia
